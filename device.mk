0aa37df716a282298dc8a4d512ad7eafe1545a19
Kevte89 <kevte89@example.com>
2025-09-29 23:37:36 +0200

    Sync latest e3q changes before GApps integration

--- device.mk
+++ device.mk
@@ -411,3 +411,16 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/matlog/privapp-permissions-matlog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-matlog.xml
+
+# USB feature permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
+# Bluetooth audio policy
+PRODUCT_COPY_FILES += \
+    vendor/samsung/e3q/proprietary/vendor/etc/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
+
+# De-dupe: wir benutzen das Vendor-BT-Audio, nicht die AOSP-AIDL-Impl
+PRODUCT_PACKAGES -= \
+    android.hardware.bluetooth.audio-impl
