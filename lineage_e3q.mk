e3a9af1d2cddb6f571550a9647865c0475b39451
David Wronek <david.wronek@mainlining.org>
2025-11-09 14:32:45 +0100

    e3q: bump to S928BXXS4CYJ7
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- lineage_e3q.mk
+++ lineage_e3q.mk
@@ -23,5 +23,5 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="e3qxxx-user 14 UP1A.231005.007 S928BXXU4BYE7 release-keys" \
-    BuildFingerprint="samsung/e3qxxx/e3q:14/UP1A.231005.007/S928BXXU4BYE7:user/release-keys"
+    BuildDesc="e3qxxx-user 14 UP1A.231005.007 S928BXXS4CYJ7 release-keys" \
+    BuildFingerprint="samsung/e3qxxx/e3q:14/UP1A.231005.007/S928BXXS4CYJ7:user/release-keys"
