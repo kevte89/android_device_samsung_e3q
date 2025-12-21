95f1c3072a7467d90f9a9d1a5121a50d13ab7d71
David Wronek <david.wronek@mainlining.org>
2025-12-21 13:33:06 +0100

    e3q: add vendor.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,5 +18,8 @@ TARGET_SCREEN_DENSITY := 600
 # Kernel
 TARGET_KERNEL_CONFIG := oem/e3q_defconfig
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Include the proprietary files BoardConfig.
 include vendor/samsung/e3q/BoardConfigVendor.mk
